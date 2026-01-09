7427f6044caff505f2757998a2695985000c7e26
João pedro Floriano <joaopflori23@gmail.com>
2026-01-09 18:31:42 -0300

    Encerramento do projeto Cria uma classe chamada Account com vários métodos e atributos, além de finalizar o main

---
+++ entities/Account.java
@@ -0,0 +1,42 @@
+package entities;
+
+
+public class Account{
+    private String name;
+    private int number;
+    private double balance;
+
+    public Account(String name, int number, double balance){
+        this.name = name;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+    
+    public void withdraw(double qtd){
+        balance -= qtd + 5;
+    }
+
+    public void deposit(double qtd){
+        balance += qtd;
+    }
+
+    public String toString(){
+        return String.format("Account: %d, Holder: %s, Balance: $ %.2f", number, name, balance);
+    }
+}
